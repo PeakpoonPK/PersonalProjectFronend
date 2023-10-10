9e6db6a810657f09031de2d00c2d20dd591b9503
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-10-10 22:08:41 +0700

    all link from HomePage

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,38 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "../page/HomePage";
+import TodayAvailable from "../page/TodayAvailable";
+import Service from "../page/Service";
+import About from "../page/About"
+import SearchBooking from "../page/SearchBooking"
+import Header from "../layout/header";
+import { Outlet } from "react-router-dom";
+import LoginPage from "../page/LogInPage"
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: (
+            <div>
+                <Header />
+                <div>
+                    < Outlet />
+                </div>
+            </div>
+        ),
+        children: [
+            { path: '/', element: <HomePage /> },
+            { path: '/today', element: <TodayAvailable /> },
+            { path: '/service', element: <Service /> },
+            { path: '/aboout', element: <About /> },
+            { path: '/login', element: <LoginPage /> },
+            { path: '/booking', element: <SearchBooking /> }
+        ]
+    }
+]);
 
 export default function Router() {
     return (
-        <div>Router</div>
+        <RouterProvider router={router} />
     )
 }
