604463bb4926fce2a4f6627edb979f9cb8e5db12
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-10-12 16:16:26 +0700

    finish all dropDown

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,6 @@
 export default function Loading() {
     return (
+        // <span className="loading loading-spinner text-error"></span>
         <div className="bg-slate-50 flex justify-center items-center h-screen text-5xl">Loading</div>
     )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,6 +5,8 @@ import { useAuth } from '../hooks/use_auth';
 export default function Header() {
     const { logout, authUser } = useAuth();
     const regularheader = 'text-semantic-textPrimary font-normal text-lg hover:cursor-pointer hover:bg-primary-lightest px-4 py-2 rounded-3xl active:bg-primary-light';
+    const dropdownDesign = 'text-white font-normal text-lg hover:cursor-pointer hover:bg-primary-dark px-4 py-2 active:bg-primary-darker';
+
     return (
         <header className='bg-slate-50 h-24 items-center flex justify-between fixed top-0 left-0 right-0 z-10'>
             <div className='flex gap-4 items-center px-16 '>
@@ -25,12 +27,25 @@ export default function Header() {
                             Today's Available
                         </Link>
                     </li>
-                    <li
-                        className={regularheader}>
-                        <Link to='/service'>
-                            Service
-                        </Link>
-                    </li>
+                    <div>
+                        <details className="dropdown dropdown-bottom">
+                            <summary className='flex justify-center items-center text-semantic-textPrimary font-normal text-lg hover:cursor-pointer hover:bg-primary-lightest px-4 py-2 rounded-3xl active:bg-primary-light'>
+                                <span>Service</span>
+                                <span className="material-symbols-outlined">keyboard_arrow_down</span>
+                            </summary>
+                            <ul tabIndex={0} className="absolute mt-4 dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-48 bg-primary-darker">
+                                <li className={dropdownDesign}>Cardiology</li>
+                                <li className={dropdownDesign}>Dentistry</li>
+                                <li className={dropdownDesign}>Dermatology</li>
+                                <li className={dropdownDesign}>Surgery</li>
+                                <li className={dropdownDesign}>Oncology</li>
+                                <li className={dropdownDesign}>Vaccination</li>
+                                <li className={dropdownDesign}>Neurology</li>
+                                <li className={dropdownDesign}>Acupuncture</li>
+                                <li className={dropdownDesign}>Ophthalmology</li>
+                            </ul>
+                        </details>
+                    </div>
                     <li
                         className={regularheader}>
                         <Link to='/about'>
@@ -38,15 +53,35 @@ export default function Header() {
                         </Link>
                     </li>
                     {authUser ?
-                        (<div className='flex justify-center items-center'>
-                            <li className='flex justify-center items-center gap-2 text-semantic-textPrimary font-normal text-lg rounded-3xl border-2 border-primary-darker hover:cursor-pointer hover:bg-primary-lightest px-4 py-2 active:bg-primary-light' >
-                                <div className=' w-8 h-8 rounded-full bg-primary-light'></div>
-                                <Link to='/profile'> {authUser.firstName}</Link>
-                            </li>
-                            <li className={regularheader} onClick={logout}>
-                                Log out
-                            </li>
-                        </div>
+                        (<details className="dropdown dropdown-bottom" >
+                            <summary className=' flex justify-center items-center gap-2 text-semantic-textPrimary font-normal text-lg rounded-3xl border-2 border-primary-darker hover:cursor-pointer hover:bg-primary-lightest px-4 py-2 active:bg-primary-light' >
+                                <span className="material-symbols-outlined w-8 h-8 p-2 rounded-full bg-primary-light text-white flex justify-center items-center text-xl font-thin">person</span>
+                                <span> {authUser.firstName}</span>
+                                <span className="material-symbols-outlined">keyboard_arrow_down</span>
+                            </summary>
+                            <ul tabIndex={0} className="absolute mt-4 dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-48 bg-primary-darker">
+                                <li className={dropdownDesign} >
+                                    <Link to="/profile">
+                                        Profile
+                                    </Link>
+                                </li>
+                                <li className={dropdownDesign} >
+                                    <Link to="/profile">
+                                        My Pets
+                                    </Link>
+
+                                </li>
+                                <li className={dropdownDesign} >
+                                    <Link to="/profile">
+                                        My Appointment
+                                    </Link>
+                                </li>
+                                <li className={dropdownDesign} onClick={logout}>
+                                    Log out
+                                </li>
+
+                            </ul>
+                        </details>
                         )
                         :
                         (<li
