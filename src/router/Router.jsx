cd72ef32d7b55b5ff61ce8e064a3f9dd1da3e67e
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-10-16 11:23:28 +0700

    redirect login

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,11 +4,11 @@ import TodayAvailable from "../page/TodayAvailable";
 import Service from "../page/Service";
 import About from "../page/About"
 import SearchBooking from "../page/SearchBooking"
-import Header from "../layout/header";
+import Header from "../layout/Header";
 import { Outlet } from "react-router-dom";
 import LoginPage from "../page/LogInPage"
 import RegisterPage from "../page/RegisterPage"
-import RedirectIfAuthenticated from "../contexts/RedirectAuthenticated";
+import RedirectIfAuthenticated from "../feature/auth/RedirectAuthenticated";
 import ProfilePage from '../page/ProfilePage'
 
 
@@ -25,18 +25,31 @@ const router = createBrowserRouter([
             </div>
         ),
         children: [
-            {
-                path: '/', element: <HomePage />
-            },
+            { path: '/', element: <HomePage /> },
             { path: '/today', element: <TodayAvailable /> },
             { path: '/service', element: <Service /> },
             { path: '/about', element: <About /> },
-            { path: '/login', element: <LoginPage /> },
+            // { path: '/login', element: <LoginPage /> },
             { path: '/booking', element: <SearchBooking /> },
             { path: '/register', element: <RegisterPage /> },
-            { path: '/profile', element: <ProfilePage /> }
+            { path: '/profile/:profileId', element: <ProfilePage /> },
+            { path: '/pets/:profileId', element: <ProfilePage /> },
+            { path: '/appointment/:profileId', element: <ProfilePage /> }
         ]
     },
+    {
+        path: '/login',
+        element: (
+            <div className="bg-slate-50 h-screen relation">
+                <Header />
+                <div>
+                    <RedirectIfAuthenticated>
+                        <LoginPage />
+                    </RedirectIfAuthenticated>
+                </div>
+            </div>
+        )
+    }
 
 ]);
 
