10c403abfaac76832bd7ab6815dd492c3ab9ab88
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-10-23 15:03:06 +0700

    finish design all appointment

--- src/feature/Appointment/AppointmentTable.jsx
+++ src/feature/Appointment/AppointmentTable.jsx
@@ -1,7 +1,48 @@
 
 
+const headerTable = [
+    { id: 1, title: 'No.' },
+    { id: 2, title: 'Time' },
+    { id: 3, title: 'Animal name' },
+    { id: 4, title: 'Owner' },
+    { id: 5, title: 'Room' },
+    { id: 6, title: 'Doctor' },
+]
+
 export default function AppointmentTable() {
     return (
-        <div>AppointmentTable</div>
+        <div>
+            <table className="table-auto text-xl text-primary-darker ">
+                <tr className="font-thin p-2 bg-semantic-lightCream ">
+                    {headerTable.map((el, headerId) => {
+                        return <th key={headerId} className="border-2 border-semantic-darkCream py-4 px-14">{el.title}</th>
+                    })}
+                </tr>
+                <tr className="border-2 border-semantic-darkCream text-center">
+                    <td>1</td>
+                    <td>9:00-9:30</td>
+                    <td>Duffie</td>
+                    <td>Micky</td>
+                    <td>Cardiology</td>
+                    <td>Peerapan Komol</td>
+                </tr>
+                <tr className="border-2 border-semantic-darkCream text-center">
+                    <td>1</td>
+                    <td>9:00-9:30</td>
+                    <td>Duffie</td>
+                    <td>Micky</td>
+                    <td>Cardiology</td>
+                    <td>Peerapan Komol</td>
+                </tr>
+                <tr className="border-2 border-semantic-darkCream text-center">
+                    <td>1</td>
+                    <td>9:00-9:30</td>
+                    <td>Duffie</td>
+                    <td>Micky</td>
+                    <td>Cardiology</td>
+                    <td>Peerapan Komol</td>
+                </tr>
+            </table>
+        </div>
     )
 }
